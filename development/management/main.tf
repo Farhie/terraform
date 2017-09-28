4307d7974de77e1ab7aeaf7ceb071f30cc0a71d3
Adam Fahie <adam.fahie@gmail.com>
2017-09-27 20:38:53 -0700

    Adam - General refactor

--- development/management/main.tf
+++ development/management/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_version = "0.10.6"
 
   backend "s3" {
-    bucket = "nordstrom-oms-development-state-storage"
+    bucket = "farhie-development-state-storage"
     key    = "management/terraform.tfstate"
     region = "us-west-2"
   }
--- development/network/main.tf
+++ development/network/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_version = "0.10.6"
 
   backend "s3" {
-    bucket  = "nordstrom-oms-development-state-storage"
+    bucket  = "farhie-development-state-storage"
     key     = "network/terraform.tfstate"
     region  = "us-west-2"
     encrypt = true
